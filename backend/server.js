5f62774d2be2ea786b200c8ec3748eed00c9f859
Daria Surganov <zelya2909@gmail.com>
2024-10-04 20:05:54 -0400

    procfile, run front end from backend

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,22 @@ const { PORT } = require("./config");
 app.listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
+
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React build folder
+app.use(express.static(path.join(__dirname, '../jobly/build')));
+
+// Catch-all route to serve index.html for all unmatched routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../jobly/build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
